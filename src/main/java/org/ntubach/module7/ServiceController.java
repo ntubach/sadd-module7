a34a42ecc0f7f4ed82e3c2d51b9186c6cf2d7f61
Niko Tubach <nikotubach@gmail.com>
2023-10-14 21:45:56 -0400

    All course services complete

--- src/main/java/org/ntubach/module7/ServiceController.java
+++ src/main/java/org/ntubach/module7/ServiceController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 // Controller for the service
@@ -25,16 +26,52 @@ public class ServiceController {
         return ResponseEntity.ok("Hello World! Niko Tubach Module7");
     }
 
-    @PostMapping(value = "/addCourse")
+    // GET a course based on courseNumber
+    @GetMapping(value = "/course")
+    public ResponseEntity<Course> getCourse(@RequestParam(value = "courseNumber") Integer courseNumber) {
+        for (Course c : courses) {
+            if (Objects.equals(c.getCourseNumber(), courseNumber))
+                return ResponseEntity.ok(c);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    // POST an additional new course and add it to the registrar
+    @PostMapping(value = "/course")
     public ResponseEntity<Course> addCourse(@RequestParam(value = "courseNumber") Integer courseNumber, @RequestParam(value = "courseTitle") String courseTitle) {
-        // Validation on newCourse as needed
+        // Validation on newCourse info as needed
         Course newCourse = new Course(courseNumber, courseTitle);
         courses.add(newCourse);
         registrar.add(new Registrar(newCourse.getCourseNumber(), new ArrayList<>()));
         return ResponseEntity.ok(newCourse);
     }
 
-    @GetMapping(value = "/all_courses")
+    // PUT and update to a specified course number
+    @PutMapping(value = "/course")
+    public ResponseEntity<Course> updateCourse(@RequestParam(value = "courseNumber") Integer courseNumber, @RequestParam(value = "courseTitle") String courseTitle) {
+        // Validation on updateCourse info as needed
+        for (int i = 0; i < courses.size(); i++) {
+            if (Objects.equals(courses.get(i).getCourseNumber(), courseNumber)) {
+                courses.set(i, new Course(courseNumber, courseTitle));
+                return ResponseEntity.ok(courses.get(i));
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    // DELETE course based on courseNumber if it exists
+    @DeleteMapping(value = "/course")
+    public ResponseEntity<Course> deleteCourse(@RequestParam(value = "courseNumber") Integer courseNumber) {
+        for (int i = 0; i < courses.size(); i++) {
+            if (Objects.equals(courses.get(i).getCourseNumber(), courseNumber)) {
+                return ResponseEntity.ok(courses.remove(i));
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    // GET all courses
+    @GetMapping(value = "/course/all")
     public ResponseEntity<List<Course>> getAllCourses() {
         return ResponseEntity.ok(courses);
     }
