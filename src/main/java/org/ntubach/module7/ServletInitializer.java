e266c516579c5921309e23270b371a0bbcb547bc
Niko Tubach <nikotubach@gmail.com>
2023-10-14 21:15:48 -0400

    small update

--- src/main/java/org/ntubach/module7/ServletInitializer.java
+++ src/main/java/org/ntubach/module7/ServletInitializer.java
@@ -8,7 +8,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 
-        System.out.println("+++++++++++++++++++ SpringApplicationBuilder ++++++++++++++++");
+        System.out.println("SpringApplicationBuilder");
 
         return application.sources(Module7Application.class);
     }
